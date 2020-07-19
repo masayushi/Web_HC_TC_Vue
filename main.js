59d7e0bbe701899f50fb54e9f25f26d656e443d1
KID421 <suchild7@gmail.com>
2020-07-19 11:59:02 +0800

    使用 Vue 寫入資料

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ console.log("我是JS");
 // 定義渲染區域
 // 變數 vue1 = new Vue(); - 要新增一個 Vue 物件
 // el 指定元素 "選取器"
+// data 資料
+// { 物件名稱: 物件值 }
 var vue1 = new Vue({
-    el: "#vue1"
+    el: "#vue1",
+    data: {
+        text: "哈囉，我是 vue ~"
+    }
 });
